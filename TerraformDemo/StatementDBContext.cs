d44f1bd73786a1d4853e058e70ac45dcec4b3cef
Thierry Paquet <thierry.paquet@ezoqc.com>
2023-01-09 18:27:12 -0500

    Ajoutez des fichiers projet.

---
+++ TerraformDemo/StatementDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TerraformDemo.Models;
+
+namespace TerraformDemo
+{
+    public class StatementDBContext : DbContext
+    {
+        public DbSet<Statement> Statements;
+    }
+}
